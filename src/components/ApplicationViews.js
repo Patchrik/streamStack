7e353becb8301b12cb2bc99b79bb22b2d3f9cd11
Parker Kelley <Pklly92@gmail.com>
2021-04-15 11:07:36 -0500

    added in the data/context provider for our routes

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,10 +1,13 @@
 import React, { useContext } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+import { DataProvider } from "../providers/DataProvider";
 
 const ApplicationViews = () => {
 	return (
 		<Switch>
-			<Route></Route>
+			<Route>
+				<DataProvider></DataProvider>
+			</Route>
 		</Switch>
 	);
 };
