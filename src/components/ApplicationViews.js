0445ada9aebf5b923043952ff9c87234b4e6f265
Parker Kelley <Pklly92@gmail.com>
2021-04-15 10:32:05 -0500

    Added in the router and a app view file.

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+const ApplicationViews = () => {
+	return (
+		<Switch>
+			<Route></Route>
+		</Switch>
+	);
+};
